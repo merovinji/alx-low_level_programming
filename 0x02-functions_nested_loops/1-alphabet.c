a8e720fcd68fb5bcfed4478ac64f7f7205410cfd
merovinji <monalisa4wealth@gmail.com>
2022-09-13 13:21:29 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,15 @@
 #include "main.h"
+/**
+ * print alphabet
+ * Return: 0
+ */
 
 void print_alphabet(void)
 {
-	char i ='a';
-	while (i <= 'z') {
-		_putchar(i);
-		 i ++;
+	char c ='a';
+	while (c <= 'z') {
+		_putchar(c);
+		 c++;
 	}
 	_putchar('\n');
 }
