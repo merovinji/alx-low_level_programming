a0be046b9064618136934ee221f0eaad26c093cc
merovinji <monalisa4wealth@gmail.com>
2022-09-14 12:51:48 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,21 +2,17 @@
 
 /**
  * print_alphabet - prints the alphabet, in lower case, followed by a new line.
+ * 
  */
-
 void print_alphabet(void)
 {
 	char ch;
 
 	ch = 'a';
-	
 	while (c <= 'z')
 	{
 		_putchar(ch);
 		ch++;
-
 	}
-
 	_putchar('\n');
-
 }
